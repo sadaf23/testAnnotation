6637955e468e240b0547013333d4f1ce48f788d5
sadaf23 <88616146+sadaf23@users.noreply.github.com>
2025-04-09 16:01:49 +0530

    src added

---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { provideRouter } from '@angular/router';
+import { LoginComponent } from  './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AnnotationComponent } from './annotation/annotation.component';
+// import { PrincipleAnnotatorComponent } from './principle-annotator/principle-annotator.component';
+
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'annotation', component: AnnotationComponent }
+  // { path: "principle-annotator", component: PrincipleAnnotatorComponent }
+];
+
+export const appRouting = provideRouter(routes);
